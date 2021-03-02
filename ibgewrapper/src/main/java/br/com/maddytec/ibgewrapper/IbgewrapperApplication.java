03582c689f41232cd5fc19ec87fe12fdf692d0ba
maddytec <maddytec@gmail.com>
2021-03-02 07:37:37 -0300

    First commit

---
+++ ibgewrapper/src/main/java/br/com/maddytec/ibgewrapper/IbgewrapperApplication.java
@@ -0,0 +1,13 @@
+package br.com.maddytec.ibgewrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbgewrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbgewrapperApplication.class, args);
+	}
+
+}
